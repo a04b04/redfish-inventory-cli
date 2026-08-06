cbc5fbeaf0a4a4e24684775867b41e3199769c92
a04b04 <arun04b@gmail.com>
2026-08-06 13:56:29 +0100

    User now types in their URL rather than having to change code in the config

--- lib/redfish_inventory/config.rb
+++ lib/redfish_inventory/config.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 module RedfishInventory
   module Config
-    API_URL = 'http://10.151.0.44:3000/api/v1'
+    API_URL =
+      ConfigStore.api_url ||
+      ENV['REDFISH_INVENTORY_API_URL'] ||
+      'http://localhost:3000/api/v1'
   end
 end
+
+
+
+ #API_URL = 'http://10.151.0.44:3000/api/v1'
